23bcbd9d2fea46aeecc8cc019cac044f1cd703fa
Alexander Savin <sha1-33fa950a793c312cbb14a2a18fe7906e68707b3c@cern.ch>
2008-09-22 14:23:37 +0000

    take files from castor

--- test/testRCToffline.py
+++ test/testRCToffline.py
@@ -11,7 +11,11 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(50)
 )
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/774/2260AF5F-356E-DD11-99F3-000423D6C8E6.root')
-)
+    fileNames = cms.untracked.vstring(
+#if you use the dbs
+'/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/774/2260AF5F-356E-DD11-99F3-000423D6C8E6.root'
+#if you use castor
+#'rfio:/castor/cern.ch/cms/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/063/135/86E93798-5B85-DD11-BE81-000423D9870C.root'
+))
 
 
