aadf86e501640bb719a111ddc9928bc4dc221f80
cmsbuild <>
2009-09-29 10:17:04 +0000

    This commit was manufactured by cvs2git to create tag 'DQM-L1TMonitor- CMSSW_3_4_0_pre3'.
    Sprout from master 2009-09-29 10:17:00 UTC Alexander Savin <sha1-33fa950a793c312cbb14a2a18fe7906e68707b3c@cern.ch> 'HLT filter for RCT is implemented'
    Cherrypick from master 2009-06-23 09:48:55 UTC Alex Tapper <sha1-11d34dd73d9f7a19fb1c333fabc2630e81f23142@cern.ch> 'Added missing occupancy plot for central and forward jets.':
        src/L1TDTTF.cc
        src/L1TGCT.cc
        src/L1TRCT.cc
        test/testEmulMon_cfg.py
    Delete:
        data/L1TdeRCTQualityTests.xml
        interface/L1TdeCSCTF.h
        src/L1TdeCSCTF.cc

--- test/testEmulMon_cfg.py
+++ test/testEmulMon_cfg.py
@@ -13,16 +13,13 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 #process.GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
 #on-line
 #process.GlobalTag.globaltag = 'CRAFT_V2H::All'
-#process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"   
-process.GlobalTag.globaltag = 'GR09_31X_V3P::All' # or any other appropriate
-process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
-
+#process.GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG'
 
 #ETP,HTP,RCT,GCT, DTP,DTF,CTP,CTF,RPC, LTC,GMT,GLT 
 #process.l1compare.COMPARE_COLLS = [0,0,0,1, 0,0,0,0,0, 0,1,0]
-process.l1compare.DumpMode = -1
-process.l1compare.VerboseFlag = 1
-process.l1demon.VerboseFlag = 1
+#process.l1compare.DumpMode = -1
+#process.l1compare.VerboseFlag = 1
+#process.l1demon.VerboseFlag = 1
 #process.l1demonecal.VerboseFlag = 1
 #process.l1demongct.VerboseFlag = 1
 #process.l1tderct.verbose = False
@@ -33,20 +30,13 @@ process.l1demon.disableROOToutput = False
 #process.l1demon.RunInFilterFarm=True
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(2)
+    input = cms.untracked.int32(20)
 )
-#process.source = cms.Source("PoolSource",
-#    fileNames = cms.untracked.vstring(
-#    '/store/data/Commissioning08/Monitor/RAW/v1/000/065/935/7E8E286F-6A99-DD11-8A15-000423D6B48C.root'
-#    )
-#)
-
-
-process.source = cms.Source("NewEventStreamFileReader",
+process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
- '/store/streamer/MWGR_29/A/000/106/019/MWGR_29.00106019.0036.A.storageManager.07.0000.dat'
- ));
-
+    '/store/data/Commissioning08/Monitor/RAW/v1/000/065/935/7E8E286F-6A99-DD11-8A15-000423D6B48C.root'
+    )
+)
 
 #process.outputEvents = cms.OutputModule("PoolOutputModule",
 #    fileName = cms.untracked.string('testdemon.root')
