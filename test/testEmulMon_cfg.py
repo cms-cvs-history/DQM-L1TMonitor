c0b04d9135cec045457d1c084d8aa0b29080c3ca
cmsbuild <>
2009-10-19 15:44:58 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- L1TMonitor-V04-04-00'.
    Sprout from master 2009-10-19 15:44:57 UTC Joseph Anthony Gartner <sha1-120ea671594459b01226f9faee5dbc168f706fa8@cern.ch> 'Removed problem in code which was causing instability.'
    Cherrypick from master 2009-06-23 09:48:55 UTC Alex Tapper <sha1-11d34dd73d9f7a19fb1c333fabc2630e81f23142@cern.ch> 'Added missing occupancy plot for central and forward jets.':
        src/L1TDTTF.cc
        src/L1TGCT.cc
        src/L1TRCT.cc
        src/SealModule.cc
        test/testEmulMon_cfg.py
        test/testRPCNew.py
    Cherrypick from master 2009-09-29 10:17:00 UTC Alexander Savin <sha1-33fa950a793c312cbb14a2a18fe7906e68707b3c@cern.ch> 'HLT filter for RCT is implemented':
        python/L1TEmulatorMonitor_cff.py
        python/L1TdeRCT_cff.py
    Delete:
        data/L1TdeRCTQualityTests.xml
        python/L1GtHwValidation_cff.py
        python/L1TdeCSCTF_cfi.py
        python/l1GtHwValidation_cfi.py
        test/L1GtHwValidation_cfg.py

--- test/testEmulMon_cfg.py
+++ test/testEmulMon_cfg.py
@@ -7,41 +7,39 @@ process.load("DQM/L1TMonitor/L1TEmulatorMonitor_cff")
 
 process.load("Configuration.StandardSequences.Geometry_cff")
 #process.load("L1Trigger.Configuration.L1Config_cff")
-
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-
+ 
 #off-line
-process.GlobalTag.globaltag = 'GR09_P_V1::All'
-#process.GlobalTag.globaltag = 'GR09_31X_V6P::All'
+#process.GlobalTag.globaltag = 'CRAFT_V2P::All'
 #process.GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
-#process.GlobalTag.globaltag = "GR09_P_V1::All"
-
+#on-line
+#process.GlobalTag.globaltag = 'CRAFT_V2H::All'
+#process.GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG'
 
 #ETP,HTP,RCT,GCT, DTP,DTF,CTP,CTF,RPC, LTC,GMT,GLT 
-process.l1compare.COMPARE_COLLS = [0,0,0,0, 0,0,0,1,0, 0,0,0]
-process.l1compare.DumpMode = -1
-process.l1compare.VerboseFlag = 1
-process.l1demon.VerboseFlag = 1
+#process.l1compare.COMPARE_COLLS = [0,0,0,1, 0,0,0,0,0, 0,1,0]
+#process.l1compare.DumpMode = -1
+#process.l1compare.VerboseFlag = 1
+#process.l1demon.VerboseFlag = 1
 #process.l1demonecal.VerboseFlag = 1
 #process.l1demongct.VerboseFlag = 1
 #process.l1tderct.verbose = False
 process.l1demon.disableROOToutput = False
-process.l1demonecal.disableROOToutput = False
-process.l1demongct.disableROOToutput = False
-process.l1tderct.disableROOToutput = False
+#process.l1demonecal.disableROOToutput = False
+#process.l1demongct.disableROOToutput = False
+#process.l1tderct.disableROOToutput = False
 #process.l1demon.RunInFilterFarm=True
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(20)
 )
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    #'file:file_craft09_fevt_110998.root'
-    '/store/express/CRAFT09/ExpressMuon/FEVT/v1/000/110/998/FEF63450-1B8A-DE11-8102-000423D987FC.root'
+    '/store/data/Commissioning08/Monitor/RAW/v1/000/065/935/7E8E286F-6A99-DD11-8A15-000423D6B48C.root'
     )
 )
 
-process.outputEvents = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('testdemon.root')
-)
-process.ep = cms.EndPath(process.outputEvents)
+#process.outputEvents = cms.OutputModule("PoolOutputModule",
+#    fileName = cms.untracked.string('testdemon.root')
+#)
+#
+#process.ep = cms.EndPath(process.outputEvents)
