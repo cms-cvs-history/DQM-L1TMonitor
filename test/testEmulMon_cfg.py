ba455b6cb1b392e63f02993b7f669ff4108d54af
cmsbuild <>
2010-03-03 17:23:52 +0000

    This commit was manufactured by cvs2git to create tag 'DQM-L1TMonitor- CMSSW_3_5_4_patch1'.
    Sprout from master 2010-03-03 17:23:50 UTC Joseph Anthony Gartner <sha1-120ea671594459b01226f9faee5dbc168f706fa8@cern.ch> '* added Test program for debugging CSCTF'
    Cherrypick from master 2009-11-03 11:38:48 UTC Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch> 'update input, instructions':
        test/testEmulMon_cfg.py

--- test/testEmulMon_cfg.py
+++ test/testEmulMon_cfg.py
@@ -6,15 +6,19 @@ process.load("DQMServices.Core.DQM_cfg")
 process.load("DQM/L1TMonitor/L1TEmulatorMonitor_cff")
 
 process.load("Configuration.StandardSequences.Geometry_cff")
+#process.load("L1Trigger.Configuration.L1Config_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
 #off-line
-process.GlobalTag.globaltag = 'GR10_P_V2::All'
+process.GlobalTag.globaltag = 'GR09_P_V1::All'
+#process.GlobalTag.globaltag = 'GR09_31X_V6P::All'
+#process.GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
+#process.GlobalTag.globaltag = "GR09_P_V1::All"
+
 
 #ETP,HTP,RCT,GCT, DTP,DTF,CTP,CTF,RPC, LTC,GMT,GLT 
 #process.l1compare.COMPARE_COLLS = [0,0,0,0, 0,0,0,1,0, 0,0,0]
-#process.l1compare.COMPARE_COLLS = [1,1,1,1, 1,1,1,0,1, 0,1,1]
 process.l1compare.DumpMode = -1
 process.l1compare.VerboseFlag = 1
 process.l1demon.VerboseFlag = 1
@@ -28,7 +32,7 @@ process.l1tderct.disableROOToutput = False
 #process.l1demon.RunInFilterFarm=True
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(100)
 )
 
 ##to access new runs in castor, use dbs eg (specify desired run number in 'run=')
@@ -36,7 +40,8 @@ process.maxEvents = cms.untracked.PSet(
 #check rfdir /castor/cern.ch/cms/store/data/Commissioning09/Cosmics/RAW/v3/000/118/969/FEA12E2E-EEC5-DE11-8BD0-000423D98C20.root
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    '/store/data/Commissioning10/Cosmics/RAW/v3/000/127/336/F6903CB5-A816-DF11-B5EA-0030487A3C9A.root'
+    #'file:file_craft09_fevt_110998.root'
+    '/store/data/Commissioning09/Cosmics/RAW/v3/000/118/969/FEA12E2E-EEC5-DE11-8BD0-000423D98C20.root'
     )
 )
 
