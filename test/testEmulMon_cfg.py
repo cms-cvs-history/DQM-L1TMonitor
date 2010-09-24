155e55c073acb3d8f790bd48f8f4432d1d7b1ab2
cmsbuild <>
2010-09-24 18:40:25 +0000

    This commit was manufactured by cvs2git to create tag 'DQM-L1TMonitor- CMSSW_3_9_2_patch2'.
    Sprout from master 2010-04-06 01:17:00 UTC Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch> 'add stable r/o config flag'
    Cherrypick from master 2010-09-24 18:40:16 UTC Marco Rovere <sha1-46e684da570041e629f20e4154a1c32d32598aaf@cern.ch> 'Changed hybridSuperClusters to cleanedHybridSuperClusters to avoid compilation error. To be cross checked with developers.':
        interface/L1TGCT.h
        interface/L1TdeGCT.h
        python/L1TECALTPG_unpack_cff.py
        python/L1THCALTPGXAna_cff.py
        python/L1THCALTPGXAna_offline_cff.py
        src/L1TGCT.cc
        src/L1TdeGCT.cc
    Cherrypick from master 2009-11-03 11:38:48 UTC Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch> 'update input, instructions':
        test/testEmulMon_cfg.py
    Cherrypick from master 2010-07-20 02:58:26 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Add missing #include files':
        src/L1GtHwValidation.cc
    Delete:
        test/l1temulator_dqm_sourceclient-live_cfg.py

--- test/testEmulMon_cfg.py
+++ test/testEmulMon_cfg.py
@@ -6,15 +6,19 @@ process.load("DQMServices.Core.DQM_cfg")
 process.load("DQM/L1TMonitor/L1TEmulatorMonitor_cff")
 
 process.load("Configuration.StandardSequences.Geometry_cff")
+#process.load("L1Trigger.Configuration.L1Config_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
 #off-line
-process.GlobalTag.globaltag = 'GR10_P_V4::All'
+process.GlobalTag.globaltag = 'GR09_P_V1::All'
+#process.GlobalTag.globaltag = 'GR09_31X_V6P::All'
+#process.GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
+#process.GlobalTag.globaltag = "GR09_P_V1::All"
+
 
 #ETP,HTP,RCT,GCT, DTP,DTF,CTP,CTF,RPC, LTC,GMT,GLT 
 #process.l1compare.COMPARE_COLLS = [0,0,0,0, 0,0,0,1,0, 0,0,0]
-#process.l1compare.COMPARE_COLLS = [1,1,1,1, 1,1,1,0,1, 0,1,1]
 process.l1compare.DumpMode = -1
 process.l1compare.VerboseFlag = 1
 process.l1demon.VerboseFlag = 1
@@ -31,13 +35,13 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
 )
 
-
 ##to access new runs in castor, use dbs eg (specify desired run number in 'run=')
 ##https://cmsweb.cern.ch/dbs_discovery/getLFN_txt?dbsInst=cms_dbs_prod_global&blockName=*&dataset=/Cosmics/Commissioning09-v3/RAW&userMode=user&run=118969&what=cff
 #check rfdir /castor/cern.ch/cms/store/data/Commissioning09/Cosmics/RAW/v3/000/118/969/FEA12E2E-EEC5-DE11-8BD0-000423D98C20.root
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    '/store/data/Commissioning10/Cosmics/RAW/v4/000/131/872/4CE9AD33-C736-DF11-B9D7-000423D952C0.root'
+    #'file:file_craft09_fevt_110998.root'
+    '/store/data/Commissioning09/Cosmics/RAW/v3/000/118/969/FEA12E2E-EEC5-DE11-8BD0-000423D98C20.root'
     )
 )
 
