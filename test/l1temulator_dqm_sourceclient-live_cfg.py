259cc49478c37e7b8e483b3b82600d2564209b6b
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2011-04-21 18:55:12 +0000

    Added options to exclude algorithm triggers from comparison data - emulator.

--- test/l1temulator_dqm_sourceclient-live_cfg.py
+++ test/l1temulator_dqm_sourceclient-live_cfg.py
@@ -1,88 +1,155 @@
+# L1 Emulator DQM sequence
+#
+#   authors previous versions - see CVS
+#
+#   V.M. Ghete 2010-10-22 revised version of L1 emulator DQM
+
+
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("L1TEmuDQMlive")
 
+
+#----------------------------
+# Event Source
+#
+
+process.load("DQM.Integration.test.inputsource_cfi")
+#
+process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(
+    SelectEvents = cms.vstring("*")
+    )
+process.EventStreamHttpReader.consumerName = 'L1TEMU DQM Consumer'
+
+
+#----------------------------
+# DQM Environment
+#
+ 
+#
 process.load("DQMServices.Core.DQM_cfg")
+
 process.load("DQMServices.Components.DQMEnvironment_cfi")
+process.dqmEnv.subSystemFolder = 'L1TEMU'
 
-#replace this from the live version, see below
-#process.load("DQM.Integration.test.inputsource_cfi")
-#process.load("DQM.Integration.test.environment_cfi")
+#
+process.load("DQM.Integration.test.environment_cfi")
+#
+# no references needed
+# replace DQMStore.referenceFileName = "L1TEMU_reference.root"
 
-
+#
 process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
+process.GlobalTag.RefreshEachRun = cms.untracked.bool(True)
 
+#
 process.load("Configuration.StandardSequences.Geometry_cff")
 
+#-------------------------------------
+# sequences needed for L1 emulator DQM
+#
+
+# standard unpacking sequence 
+process.load("Configuration.StandardSequences.RawToDigi_Data_cff")    
+
+# L1 data - emulator sequences 
 process.load("DQM.L1TMonitor.L1TEmulatorMonitor_cff")    
 process.load("DQM.L1TMonitorClient.L1TEMUMonitorClient_cff")    
 
-#NL//this over-writting may be employed only when needed
-#  ie quick module disabling, before new tags can be corrected)
-from L1Trigger.HardwareValidation.L1HardwareValidation_cff import *
-##NL//ctp temporarily disabled (infinite time sorting too large collections)
-#l1compare.COMPARE_COLLS = [1, 1, 1, 1,  1, 1, 0, 1, 1, 0, 1, 1]
-#newHWSequence = cms.Sequence(deEcal+deHcal+deRct+deGct+deDt+deDttf+deCsc+deCsctf+deRpc+deGmt+deGt*l1compare)
-#process.globalReplace("L1HardwareValidation", newHWSequence)
+#-------------------------------------
+# paths & schedule for L1 emulator DQM
+#
 
-#N//needs to be removed from here
-process.load("SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_readDBOffline_cff")
-#process.load("SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_craft_cff")
-#process.EcalTrigPrimESProducer.DatabaseFile = 'TPG_craft.txt.gz' 
+# TODO define a L1 trigger L1TriggerRawToDigi in the standard sequence 
+# to avoid all these remove
+process.rawToDigiPath = cms.Path(process.RawToDigi)
+#
+process.RawToDigi.remove("siPixelDigis")
+process.RawToDigi.remove("siStripDigis")
+process.RawToDigi.remove("scalersRawToDigi")
+process.RawToDigi.remove("castorDigis")
+
+# L1HvVal + emulator monitoring path
+process.l1HwValEmulatorMonitorPath = cms.Path(process.l1HwValEmulatorMonitor)
+
+#
+process.l1EmulatorMonitorClientPath = cms.Path(process.l1EmulatorMonitorClient)
+
+#
+process.l1EmulatorMonitorEndPath = cms.EndPath(process.dqmEnv*process.dqmSaver)
+
+#
+
+#
+process.schedule = cms.Schedule(process.rawToDigiPath,
+                                process.l1HwValEmulatorMonitorPath,
+                                process.l1EmulatorMonitorClientPath,
+                                process.l1EmulatorMonitorEndPath)
+
+#---------------------------------------------
+
+# examples for quick fixes in case of troubles 
+#    please do not modify the commented lines
+#
+# remove a module from hardware validation
+# cff file: L1Trigger.HardwareValidation.L1HardwareValidation_cff
+#
+# process.L1HardwareValidation.remove("deCsctf")
+#
 
 
-
-## Subsystem masking in summary map (case insensitive):
-## l1t: all, gt, muons, jets, taujets, isoem, nonisoem, met
-process.l1temuEventInfoClient.dataMaskedSystems =cms.untracked.vstring("All")
-## Available emulator masks (case insensitive):
-## l1temul: "all"; "dttf", "dttpg", "csctf", "csctpg", "rpc", "gmt", "ecal", "hcal", "rct", "gct", "glt"
-process.l1temuEventInfoClient.emulatorMaskedSystems = cms.untracked.vstring("dttf", "dttpg", "csctf", "csctpg", "rpc", "ecal", "hcal", "glt")
-
-##no references needed
-#replace DQMStore.referenceFileName = "L1TEMU_reference.root"
-
-#process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(
-#    SelectEvents = cms.vstring("*")
-#)
-#process.EventStreamHttpReader.consumerName = 'L1TEMU DQM Consumer'
-process.dqmEnv.subSystemFolder = 'L1TEMU'
+#
+# remove a L1 trigger system from the comparator integrated in hardware validation
+# cfi file: L1Trigger.HardwareValidation.L1Comparator_cfi
+#
+# process.l1compare.COMPARE_COLLS = [1, 1, 1, 1,  1, 1, 1, 1, 1, 0, 1, 1]
+#
 
 
-#### additions
+#
+# remove an expert module for L1 trigger system
+# cff file: DQM.L1TMonitor.L1TEmulatorMonitor_cff
+#
+# process.l1ExpertDataVsEmulator.remove(process.l1GtHwValidation)
+#
 
-process.DQMStore.verbose = 0
-process.dqmSaver.convention = 'Online'
-process.dqmSaver.dirName = '.'
-process.dqmSaver.producer = 'DQM'
-process.dqmEnv.subSystemFolder = 'L1TEMU'
-process.dqmSaver.saveByRun = -1
-process.dqmSaver.saveAtJobEnd = False
+#process.l1ExpertDataVsEmulator.remove(process.l1TdeCSCTF)
 
-process.l1compare.DumpMode = -1
-process.l1compare.VerboseFlag = 1
-process.l1demon.VerboseFlag = 1
-#process.l1demonecal.VerboseFlag = 1
-#process.l1demongct.VerboseFlag = 1
-#process.l1tderct.verbose = False
-process.l1demon.disableROOToutput = False
-process.l1demonecal.disableROOToutput = False
-process.l1demongct.disableROOToutput = False
-process.l1tderct.disableROOToutput = False
-#process.l1demon.RunInFilterFarm=True
+#
+# remove a module / sequence from l1EmulatorMonitorClient
+# cff file: DQM.L1TMonitorClient.L1TEmulatorMonitorClient_cff
+#
+# process.l1EmulatorMonitorClient.remove(process.l1EmulatorErrorFlagClient)
+#
 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
-)
-#process.source = cms.Source("PoolSource",
-process.source = cms.Source("NewEventStreamFileReader",
-    fileNames = cms.untracked.vstring(
-    #'/store/data/BeamCommissioning09/Cosmics/RAW/v1/000/120/020/CE4D0EF0-47CC-DE11-BF95-0030487C6062.root'
- 'file:/lookarea_SM/Data.00129937.0001.A.storageManager.00.0000.dat'
-    #'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat'
-    )
-)
 
-#ETP,HTP,RCT,GCT, DTP,DTF,CTP,CTF,RPC, LTC,GMT,GLT 
-process.l1compare.COMPARE_COLLS = [0,0,0,0, 0,0,0,0,0, 0,1,0]
-#process.l1compare.COMPARE_COLLS = [1,1,1,1, 1,1,1,0,1, 0,1,1]
+#
+# fast over-mask a system in L1TEMUEventInfoClient: 
+#   if the name of the system is in the list, the system will be masked
+#   (the default mask value is given in L1Systems VPSet)             
+#
+# names are case sensitive, order is irrelevant
+# "ECAL", "HCAL", "RCT", "GCT", "DTTF", "DTTPG", "CSCTF", "CSCTPG", "RPC", "GMT", "GT"
+#
+# process.l1temuEventInfoClient.MaskL1Systems = cms.vstring("ECAL")
+#
+
+
+#
+# fast over-mask an object in L1TEMUEventInfoClient:
+#   if the name of the object is in the list, the object will be masked
+#   (the default mask value is given in L1Objects VPSet)             
+#
+# names are case sensitive, order is irrelevant
+# 
+# "Mu", "NoIsoEG", "IsoEG", "CenJet", "ForJet", "TauJet", "ETM", "ETT", "HTT", "HTM", 
+# "HfBitCounts", "HfRingEtSums", "TechTrig", "GtExternal
+#
+# process.l1temuEventInfoClient.MaskL1Objects =  cms.vstring("ETM")   
+#
+
+
+#
+# turn on verbosity in L1TEMUEventInfoClient
+#
+# process.l1EmulatorEventInfoClient.verbose = cms.untracked.bool(True)
